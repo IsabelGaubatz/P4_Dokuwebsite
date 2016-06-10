635c06bffd5add9395d6c3325b91e42aa411e63b
Olga Zimmermann <Olga@Olgas-MacBook-Air.local>
2016-06-10 16:43:22 +0200

    Doku Version 0.1
    - funktionierende Buttons
    - Popup funktioniert

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
    $(document).ready(function() {
+   	
     $('.menubutton').click(function() {
        $('nav').slideToggle('slow');
+    });
+
+    $('#contentSummary').popup();
+
+    $('#contentSummary').popup({
+        background:true
+    });
+
+    $('.btn').click(function(){
+    	$('#contentSummary').popup('show');
+    }); 
+
+    $('.my_popup_close').click(function(){
+    	$('#contentSummary').popup('hide');
     });
    });
